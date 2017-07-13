9b63a4423a07bc10977023eb31c30d705720b58b
Jurisnakib <m.nakib.cse@ulab.edu.bd>
2017-07-13 07:43:18 +0600

    Added new apps sample Xamarin form applications

---
+++ DefuseBomb/DefuseBomb/DefuseBomb/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DefuseBomb
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int Scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded", "Game Over", "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+            else
+            {
+                Scores += 1;
+                await DisplayAlert("Bomb Defused", "Scores: " + Scores, "Continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+    }
+}
